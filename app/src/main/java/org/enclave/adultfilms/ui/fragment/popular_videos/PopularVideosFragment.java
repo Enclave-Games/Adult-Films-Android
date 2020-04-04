3af8d882cc2ff6b3e6a950bcb078324307600bb4
ArtemGord1728 <artemseraf01@mail.ru>
2020-04-04 22:21:55 +0300

    Renamed some files

--- app/src/main/java/org/enclave/adultfilms/ui/fragment/popular_videos/PopularVideosFragment.java
+++ app/src/main/java/org/enclave/adultfilms/ui/fragment/popular_videos/PopularVideosFragment.java
@@ -4,32 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import org.enclave.adultfilms.R;
 
 public class PopularVideosFragment extends Fragment {
 
-    private PopularVideosModel dashboardViewModel;
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        View root = inflater.inflate(R.layout.fragment_popular_videos, container, false);
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        dashboardViewModel =
-                ViewModelProviders.of(this).get(PopularVideosModel.class);
-        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
         return root;
     }
 }
