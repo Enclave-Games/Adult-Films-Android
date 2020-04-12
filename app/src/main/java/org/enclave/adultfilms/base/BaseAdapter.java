d51f7529c1bfe39827ad339ed9c013c23cfe102b
ArtemGord1728 <artemseraf01@mail.ru>
2020-04-12 19:19:48 +0300

    Refactored base adapter

--- app/src/main/java/org/enclave/adultfilms/base/BaseAdapter.java
+++ app/src/main/java/org/enclave/adultfilms/base/BaseAdapter.java
@@ -1,25 +1,30 @@
 package org.enclave.adultfilms.base;
 
+import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class BaseAdapter extends RecyclerView.Adapter
+import org.enclave.adultfilms.R;
+import org.enclave.adultfilms.model.Videos;
+import org.enclave.adultfilms.view.VideoViewHolder;
+
+import java.util.List;
+
+public abstract class BaseAdapter extends RecyclerView.Adapter<VideoViewHolder>
 {
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public VideoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new VideoViewHolder(LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.video_item, parent, false));
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull VideoViewHolder holder, int position) {
 
     }
 
-    @Override
-    public int getItemCount() {
-        return 0;
-    }
+    public abstract void addItems(List<Videos> items);
 }
